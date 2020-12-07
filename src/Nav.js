513fc31e3afcbc00307f99cea762e39efcca8e29
Rakesh Biswal <boomshotop@gmail.com>
2020-12-07 16:06:02 +0530

    Netflix logo correction in Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -19,7 +19,7 @@ function Nav() {
         <div className={`nav ${show && "nav__black"}`}>
           <img
             className="nav__logo"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Logo_Netflix.png/800px-Logo_Netflix.png"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
             alt="Netflix Logo"
             />  
 
